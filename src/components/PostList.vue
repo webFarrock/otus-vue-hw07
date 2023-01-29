78d69673a6e38e2ba01b89ae541b5b1302165313
pavel shcheglov <shcheglov_py@magnit.ru>
2023-01-29 22:52:41 +0300

    posts desc sorting by id

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -7,10 +7,11 @@ import FilterComponent from '@/components/FilterComponent.vue'
 
 const postsStore = usePostsStore()
 const {posts} = storeToRefs(postsStore)
+const sortedPosts = computed(() => [...posts.value].sort((a, b) => b.id - a.id))
 const filter = ref('')
 const displayPosts = computed(() => {
-  if (!filter.value) return posts.value
-  return posts.value
+  if (!filter.value) return sortedPosts.value
+  return sortedPosts.value
       .filter(item => {
         if (filter.value) return queryFound(item.title, filter.value)
         return true
@@ -19,6 +20,7 @@ const displayPosts = computed(() => {
         ...item,
         title: searchHighlight(item.title, filter.value)
       }))
+      .sort((a, b) => a.id - b.id)
 })
 const handleFilter = (v: string) => filter.value = v
 </script>
@@ -26,12 +28,24 @@ const handleFilter = (v: string) => filter.value = v
 <template>
   <h1>Post list</h1>
   <FilterComponent @filter="handleFilter" title="Filter post"/>
-  <ul class="flex sm:flex-col overflow-hidden content-center justify-between posts-nav">
-    <li class="pb-2 posts-nav__item"
+  <ul class="posts">
+    <li class="posts__item"
         v-for="{id, title, selected} in displayPosts" :key="id"
         :class="{selected}"
     >
+      [{{ id }}]
       <router-link :to="postPath(id)" v-html="title"/>
     </li>
   </ul>
 </template>
+
+<style scoped lang="scss">
+.posts {
+  list-style-type: none;
+  padding-left: 0;
+
+  &__item {
+    margin-bottom: 5px;
+  }
+}
+</style>
